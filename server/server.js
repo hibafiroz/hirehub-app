5a355269a236f430b177c96eb8378af7725197f6
Hiba Firoz <hibafiroz06@gmail.com>
2026-05-07 23:52:41 +0530

    server modified for vercel

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://hirehub-app-psi.vercel.app',
   credentials: true
 }))
 
